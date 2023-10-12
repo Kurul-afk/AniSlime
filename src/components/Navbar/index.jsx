b857953dd85b23d31de3d564a7bcb813f921db99
Kurul-afk <arsenshergaziev@gmail.com>
2023-10-12 19:58:53 +0600

    clear all

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,26 +1,8 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Button, TextField, colors } from "@mui/material";
-import './style.css'
+import "./style.css";
 
 const Navbar = () => {
-
-  const navigate = useNavigate()
-  
-  return (
-  <div className="navbar">
-    <div className="navbar__container">
-      <div className="navbar__left">
-        <Link className="navbar__link" to="/">Главная</Link>
-        <Link className="navbar__link" to="/anime-list">Жанр</Link>
-      </div>
-      <div className="navbar__right">
-        <TextField className="navbar__textField" id="filled-basic" label="Поиск" variant="filled" />
-        <Button variant="contained" color='success' onClick={()=> navigate('/sign-up')}>Primary</Button>
-      </div>
-    </div>
-  </div>
-  );
+  return <div></div>;
 };
 
 export default Navbar;
